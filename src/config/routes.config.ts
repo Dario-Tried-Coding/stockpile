f06ccb8d230cf95f78cc5bfa5a02d27300cb33cd
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2024-02-24 11:57:42 +0100

    middleware

---
+++ src/config/routes.config.ts
@@ -0,0 +1,7 @@
+export const publicRoutes: string[] = []
+
+export const authRoutes: string[] = ['/auth/login', '/auth/register', '/auth/error', '/auth/reset', '/auth/new-password', '/auth/verify']
+
+export const apiAuthPrefixes: string[] = ['/api/auth', '/api/trpc/auth']
+
+export const DEFAULT_LOGIN_REDIRECT: string = '/'
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,46 @@
+import { i18nConfig } from '@/config/i18n.config'
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefixes, authRoutes, publicRoutes } from '@/config/routes.config'
 import { i18nMiddleware } from '@/lib/common/i18n/middleware'
 import { auth } from '@/lib/server/auth'
 
-export default auth(req => {
-  console.log('middleware ->', req.nextUrl.pathname, !!req.auth)
+const { locales } = i18nConfig
+
+function getLocalePrefix(pathname: string) {
+  const segments = pathname.split('/')
+
+  if (segments[0] === '') segments.shift()
+
+  if (locales.some(locale => locale === segments[0])) return segments[0]
+
+  return ''
+}
+
+export default auth((req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+
+  const localePrefix = getLocalePrefix(nextUrl.pathname)
+  // console.log('middleware ->', {pathname: nextUrl.pathname, isLoggedIn, localePrefix})
+
+  const isApiAuthRoute = apiAuthPrefixes.some((prefix) => nextUrl.pathname.startsWith(prefix))
+  const isPublicRoute = publicRoutes.some((prefix) => nextUrl.pathname.includes(prefix))
+  const isAuthRoute = authRoutes.some((prefix) => nextUrl.pathname.includes(prefix))
+
+  if (isApiAuthRoute) {
+    return
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) return Response.redirect(new URL(`${localePrefix}/${DEFAULT_LOGIN_REDIRECT}`, nextUrl))
+    return i18nMiddleware(req)
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname
+    if (nextUrl.search) callbackUrl += nextUrl.search
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+    return Response.redirect(new URL(`${localePrefix}/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl))
+  }
 
   return i18nMiddleware(req)
 })
