272a9906aba3f04e6f14bee609eb592c64175051
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2024-03-14 13:20:41 +0100

    users table - view options - i18n

--- src/components/table/TableViewOptions.tsx
+++ src/components/table/TableViewOptions.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/Button'
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/DropdownMenu'
 import { Table } from '@tanstack/react-table'
 import { Settings2 } from 'lucide-react'
+import { useTranslations } from 'next-intl'
 import { FC } from 'react'
 
 interface TableViewOptionsProps<TData> {
@@ -9,16 +10,18 @@ interface TableViewOptionsProps<TData> {
 }
 
 function TableViewOptions<TData>({ table }: TableViewOptionsProps<TData>) {
+  const t = useTranslations('Pages.Users.Table.Client.Toolbar.ViewOptions')
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='sm' className='ml-auto hidden h-8 lg:flex'>
           <Settings2 className='mr-2 h-4 w-4' />
-          View
+          {t('button')}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end' className='w-[150px]'>
-        <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+        <DropdownMenuLabel>{t('label')}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         {table
           .getAllColumns()
